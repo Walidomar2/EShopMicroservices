4224d9b96b88a6b6093a3b28b21c44afff22ef47
Walid Omar <walidomar291@gmail.com>
2024-12-01 22:29:18 +0200

    creating Vertical Slice Features structure for products

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public string Name { get; private set; } = default!;
+        public List<string> Category { get; private set; } = new();
+        public string Description { get; private set; } = default!;
+        public string ImageFile { get; private set; } = default!;
+        public decimal Price { get; private set; }
+    }
+}
