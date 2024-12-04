aa2b90eb59828df6adb92ca608b4cb4fac4674fc
Walid Omar <walidomar291@gmail.com>
2024-12-04 16:41:07 +0200

    adding marten library and setting the connection string to postgre

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,7 +4,7 @@
          : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid id);
 
-    internal class CreateProductCommandHandler 
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -19,7 +19,10 @@
                 Price = command.Price
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return  new  CreateProductResult(product.Id);
         }
     }
 }
