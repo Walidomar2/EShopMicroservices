d917e2d8dd169f343100bb7cda196d9f25504e95
Walid Omar <walidomar291@gmail.com>
2025-01-03 22:22:46 +0200

    adding fluent validation for create product command

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,11 +4,20 @@
          : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session)
+    internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            // Validation for the incoming model
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+            if (errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
+
             // Create new Product entity from command object
             var product = new Product
             {
